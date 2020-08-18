5313df7085a745b8c088a7fd82660de4b764405c
MikeYeh <mikeyeh40709@gmail.com>
2020-08-18 14:26:52 +0800

    加入專案檔案。

---
+++ Xboox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Xboox.MvcApplication" Language="C#" %>
---
+++ Xboox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Xboox.Startup))]
+namespace Xboox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
