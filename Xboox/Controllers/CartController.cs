a2c705efe815b985ba0e7d59929c02e0aaa7204f
PG <frankpg267@gmail.com>
2020-08-21 14:55:19 +0800

    新增Cart Controller和View

---
+++ Xboox/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Xboox.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Bill()
+        {
+            return View();
+        }
+    }
+}
