d33542b46b9d72a78314adc5c2531c80581a1291
Fengche Yen <knight618134@gmail.com>
2020-08-22 16:24:13 +0800

    edit cart.js content  and edit cart.scss

--- Xboox/ProjectJS/CartPage/cart.js
+++ Xboox/ProjectJS/CartPage/cart.js
@@ -1,14 +1,49 @@
-﻿//document.querySelector("#check").click(function () {
-//    if (document.querySelector("#check").checked) {
-//        alert("Abc");
-//    }
-    
-//})
-
-
-
+﻿
 let check = document.querySelector("#check");
+let title = document.querySelector(".container .row .col-sm-12");
+let card_section = document.querySelector(".card-section");
+let h3 = document.createElement('h3');
+card_section.setAttribute('style', 'display:none');
+h3.innerText = "Your basket is currently empty.";
+title.appendChild(h3);
 check.addEventListener('click', () => {
     if (check.checked) {
-        alert("A");
-    }})
+        h3.setAttribute('style', 'display:none');
+        card_section.setAttribute('style', 'display:block');
+    }
+    else {
+        h3.setAttribute('style', 'display:block');
+        card_section.setAttribute('style', 'display:none');
+    }
+    
+})
+
+//setting book price
+
+let price = document.querySelector('#price');
+let price_num = parseInt(67.00);
+price.innerHTML = `$${price_num}`;
+
+let caretleft = document.querySelector('.fa-caret-left');
+let count = document.querySelector('#count');
+let total = document.querySelector('#total');
+let caretright=document.querySelector('.fa-caret-right');
+let changeCount = () => {
+    caretleft.addEventListener('click', () => {
+        if (count.innerText > 1) {
+
+            count.innerText--;
+            
+        }
+    })
+
+    caretright.addEventListener('click', () => {
+        count.innerText++;
+        total.innerHTML = `${parseInt(price_num) * parseInt(count.inerText)
+    }`;
+    }
+        )
+    }
+changeCount();
+
+
