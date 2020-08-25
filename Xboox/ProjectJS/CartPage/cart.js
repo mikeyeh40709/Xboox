26a94dd1022b7d05ef7b354708b589b720718327
Fengche Yen <knight618134@gmail.com>
2020-08-25 13:41:11 +0800

    1. edit cart.js and bill.js 2. edit product.cshtml

--- Xboox/ProjectJS/CartPage/cart.js
+++ Xboox/ProjectJS/CartPage/cart.js
@@ -3,17 +3,29 @@ let check = document.querySelector("#check");
 let title = document.querySelector(".container .row .col-sm-12");
 let card_section = document.querySelector(".card-section");
 let h3 = document.createElement('h3');
+let basket = document.querySelector('.basket');
+let proceed = document.querySelector('.proceed');
+//coupon badge
+let badge = document.querySelector('.badge');
+
+//
 card_section.setAttribute('style', 'display:none');
+basket.setAttribute('style', 'display:none');
+proceed.setAttribute('style', 'display:none');
 h3.innerText = "Your basket is currently empty.";
 title.appendChild(h3);
 check.addEventListener('click', () => {
     if (check.checked) {
         h3.setAttribute('style', 'display:none');
         card_section.setAttribute('style', 'display:block');
+        basket.setAttribute('style', 'display:block');
+        proceed.setAttribute('style', 'display:block');
     }
     else {
         h3.setAttribute('style', 'display:block');
         card_section.setAttribute('style', 'display:none');
+        basket.setAttribute('style', 'display:none');
+        proceed.setAttribute('style', 'display:none');
     }
     
 })
@@ -21,7 +33,7 @@ check.addEventListener('click', () => {
 //setting book price
 
 let price = document.querySelector('.price');
-let price_num = parseInt(67.00);
+let price_num = parseFloat(67.00);
 price.innerHTML = `$${price_num}`;
 
 let caretleft = document.querySelector('.fa-caret-left');
@@ -49,3 +61,15 @@ total_group.forEach((element, idx) => { total_group[idx].innerHTML = `$${price_n
 changeCount();
 
 
+////proceed  click to next page
+proceed.addEventListener('click', () => { location.href = 'Bill' });
+
+//coupon badge
+let coupon_price = document.querySelector('.coupon_price');
+badge.addEventListener('click', () => {
+    total_group[1].style.textDecoration = "line-through";
+    coupon_price.innerText = `$${(Math.ceil(price_num * count.innerText * 0.8))}`;
+
+
+
+});
