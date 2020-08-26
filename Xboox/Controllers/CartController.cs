de32dcce98bd0af9a08abe725e9a20faddaac23d
PG <frankpg267@gmail.com>
2020-08-26 18:33:02 +0800

    Controller 新增 cartItem資料表

--- Xboox/Controllers/CartController.cs
+++ Xboox/Controllers/CartController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Xboox.Models;
+using XbooxCMS.Models.ViewModels;
 
 namespace Xboox.Controllers
 {
     public class CartController : Controller
     {
+        private XBooxContext _context;
+        public CartController()
+        {
+            if (_context == null)
+            {
+                _context = new XBooxContext();
+            }
+        }
         // GET: Cart
         public ActionResult Product()
         {
+            var cartItems = _context.CartItmes.ToList();
+            List<CartViewModel> carts = new List<CartViewModel>();
+            //foreach(var item in cartItems)
+            //{
+            //    carts.Add(new CartViewModel()
+            //    {
+            //        Name 
+            //    });
+            //}
             return View();
         }
         public ActionResult Bill()
