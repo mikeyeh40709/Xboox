4aca2cc5d635dfc561dc36df9f161de250cb760b
MikeYeh <mikeyeh40709@gmail.com>
2020-08-28 19:26:13 +0800

    移除TemplateScss, 修改HomePage的Scss檔, 移除Cart的ViewStart檔, 移除Shared的Layout檔, 移除SingIn的ViewStart檔 新增產品頁面PartialView, 新增HomeController的ProductDetail Action, 新增第一筆中文書圖片, 新增ProductViewModel以連結資料庫內容

--- Xboox/Controllers/HomeController.cs
+++ Xboox/Controllers/HomeController.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Xboox.Models;
+using Xboox.ViewModels;
 
 namespace Xboox.Controllers
 {
     public class HomeController : Controller
     {
+        private XbooxContext context = new XbooxContext();
         public ActionResult Index()
         {
+            
             return View();
         }
-
+        public ActionResult ProductDetail()
+        {
+            var result = context.Product;
+            var productimgs = context.ProductImgs;
+            List<ProductDetailViewModel> item = new List<ProductDetailViewModel>();
+            var query = from p in result
+                        join pi in productimgs
+                        on p.ProductImgId equals pi.ProductImgId
+                        select new ProductDetailViewModel
+                        {
+                            Name = p.Name,
+                            Quantity = p.Quantity,
+                            ISBN = p.ISBN,
+                            Price = p.Price,
+                            Publisher = p.Publisher,
+                            Intro = p.Intro,
+                            PublishedDate = p.PublishedDate,
+                            imgLink = pi.imgLink
+                        };
+            foreach(var product in query)
+            {
+                item.Add(product);
+            }
+            return View(item);
+        }
+       
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
