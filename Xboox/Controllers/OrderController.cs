40168310cb92f5f81345e51f3cf90a139dc639c5
PG <frankpg267@gmail.com>
2020-08-29 23:39:09 +0800

    新增客製化Order.css 增加OrderService控管Order方法 新增OrderDetails和Order ViewModel 增加Manger.cshtml 增加UserView

---
+++ Xboox/Controllers/OrderController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xboox.Models;
+using Xboox.Models.ViewModels;
+using Xboox.Services;
+
+namespace Xboox.Controllers
+{
+    public class OrderController : Controller
+    {
+        private XbooxContext context;
+        OrderService service = new OrderService();
+        public OrderController()
+        {
+            if(context == null)
+            {
+                context = new XbooxContext();
+            }
+        }
+        // GET: Order
+        public ActionResult UserView(string userName)
+        {
+            
+            return View();
+        }
+        public ActionResult ManagerView()
+        {
+            var result = service.GetOrder();
+            return View(result);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // 後臺會拿到所有使用者訂單
+        public ActionResult GetAllOrder()
+        {
+            return View();
+        }
+        // 使用者拿到自己的訂單
+        public ActionResult GetOrder(Guid? id)
+        {
+            return View();
+        }
+
+        // 編輯付款狀態(後台)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangeState(Guid? id)
+        {
+            return View();
+        }
+        // 刪除某筆訂單(使用者和後台)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteOrder()
+        {
+            return View();
+        }
+    }
+}
---
+++ Xboox/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Xboox.ViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string Imagelink { get; set; }
+        public string Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
