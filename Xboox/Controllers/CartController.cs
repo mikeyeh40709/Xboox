32045038aac99b58667b4ee8d34ee0761e8cbec4
Fengche Yen <knight618134@gmail.com>
2020-08-29 13:57:33 +0800

    1.增加了ShoppingCartManage類別 2.更改了CartController 3.購物車畫面調整完畢 4.更名cart.js --> shop-cart.js

--- Xboox/Controllers/CartController.cs
+++ Xboox/Controllers/CartController.cs
@@ -4,39 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Xboox.Models;
-using Xboox.Models.ViewModels;
+using Xboox.ViewModels;
 
 namespace Xboox.Controllers
 {
     public class CartController : Controller
     {
-        private XbooxContext _context;
+        private XbooxContext context;
         public CartController()
         {
-            if (_context == null)
+            if (context == null)
             {
-                _context = new XbooxContext();
+                context = new XbooxContext();
             }
         }
         // GET: Carts
 
         public ActionResult ShopCart()
         {
-            //var cartItems = _context.CartItmes.ToList();
-            List<CartViewModel> carts = new List<CartViewModel>()
-            {
-                new CartViewModel{Name="Wellness And Paradise",ProductImg="Wellnes.png",Total=67},
-                new CartViewModel{Name="Wellness And Paradise",ProductImg="Wellnes.png",Total=67}
-            };
-
-            //foreach(var item in cartItems)
+            ////var cartItems = _context.CartItmes.ToList();
+            //List<CartViewModel> carts = new List<CartViewModel>()
             //{
-            //    carts.Add(new CartViewModel()
-            //    {
-            //        Name 
-            //    });
-            //}
-            return View(carts);
+            //    new CartViewModel{Name="Wellness And Paradise",ProductImg="Wellnes.png",Total=67},
+            //    new CartViewModel{Name="Wellness And Paradise",ProductImg="Wellnes.png",Total=67}
+            //};
+
+            //return View(carts);
+
+
+
+            var result = context.Product;
+            var productimgs = context.ProductImgs;
+            List<ProductDetailViewModel> item = new List<ProductDetailViewModel>();
+            var query = from p in result
+                        join pi in productimgs
+                        on p.ProductImgId equals pi.ProductImgId
+                        select new ProductDetailViewModel
+                        {
+                            Name = p.Name,
+                            Quantity = p.Quantity,
+                            Price = p.Price,
+                            imgLink = pi.imgLink
+                        };
+            foreach (var product in query)
+            {
+                item.Add(product);
+            }
+            return View(item);
+
+
         }
 
 
