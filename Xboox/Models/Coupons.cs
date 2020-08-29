4581e6061ef602edc35d9c238fe0378fbf89c450
Sharon Chen <treetwood@gmail.com>
2020-08-29 11:06:21 +0800

    Changed database

--- Xboox/Models/Coupons.cs
+++ Xboox/Models/Coupons.cs
@@ -8,30 +8,18 @@ namespace Xboox.Models
 
     public partial class Coupons
     {
-        [Key]
-        [Column(Order = 0)]
         public Guid Id { get; set; }
 
-        [Key]
-        [Column(Order = 1)]
         [StringLength(50)]
         public string CouponName { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
-        public decimal Discount { get; set; }
+        public decimal? Discount { get; set; }
 
-        [Key]
-        [Column(Order = 3)]
         [StringLength(50)]
         public string CouponCode { get; set; }
 
-        [Key]
-        [Column(Order = 4)]
-        public DateTime StartTime { get; set; }
+        public DateTime? StartTime { get; set; }
 
-        [Key]
-        [Column(Order = 5)]
-        public DateTime EndTime { get; set; }
+        public DateTime? EndTime { get; set; }
     }
 }
