180867aaf9051bd0943c9c7f0ec08888859fd2a0
Joey Wnag <joeywang1991@gmail.com>
2020-09-07 18:08:02 +0800

    資料整併

---
+++ Xboox/Controllers/AspNetUsersCRUDController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Xboox.Models.DataTable;
+
+namespace Xboox.Controllers
+{
+    public class AspNetUsersCRUDController : Controller
+    {
+        private XbooxContext db = new XbooxContext();
+
+        // GET: AspNetUsersCRUD
+        public ActionResult Index()
+        {
+            return View(db.AspNetUsers.ToList());
+        }
+
+        // GET: AspNetUsersCRUD/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUsers aspNetUsers = db.AspNetUsers.Find(id);
+            if (aspNetUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUsers);
+        }
+
+        // GET: AspNetUsersCRUD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetUsersCRUD/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] AspNetUsers aspNetUsers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AspNetUsers.Add(aspNetUsers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aspNetUsers);
+        }
+
+        // GET: AspNetUsersCRUD/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUsers aspNetUsers = db.AspNetUsers.Find(id);
+            if (aspNetUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUsers);
+        }
+
+        // POST: AspNetUsersCRUD/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] AspNetUsers aspNetUsers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetUsers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetUsers);
+        }
+
+        // GET: AspNetUsersCRUD/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUsers aspNetUsers = db.AspNetUsers.Find(id);
+            if (aspNetUsers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUsers);
+        }
+
+        // POST: AspNetUsersCRUD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AspNetUsers aspNetUsers = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Remove(aspNetUsers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
