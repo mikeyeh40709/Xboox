636ba34ae6a4854b43272bf81e96b2022cb05b70
Sharon Chen <treetwood@gmail.com>
2020-09-11 15:46:25 +0800

    XbooxLibrary

---
+++ XbooxLibrary/Repository/GeneralRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xboox.Models.DataTable;
+
+namespace XbooxLibrary.Repository
+{
+    public class GeneralRepository<T> where T :class
+    {
+        private XbooxContext context;
+        protected XbooxContext Context
+        {
+            get { return context; }
+        }
+
+        public GeneralRepository(XbooxContext contexts)
+        {
+            if (contexts == null)
+            {
+                throw new ArgumentNullException();
+            }
+            context = contexts;
+        }
+
+        public void Create(T entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+        }
+        public void Delete(T entity)
+        {
+            context.Entry(entity).State = EntityState.Deleted;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+
+    }
+}
