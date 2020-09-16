78948fd0c0b30c65da758cdc7e431278a7eae8f3
PG <frankpg267@gmail.com>
2020-09-16 16:48:21 +0800

    更新資料庫Data欄位並更新Models

--- Xboox/Models/DataTable/CartItems.cs
+++ Xboox/Models/DataTable/CartItems.cs
@@ -8,16 +8,13 @@ namespace Xboox.Models.DataTable
 
     public partial class CartItems
     {
+        public Guid Id { get; set; }
+
         public Guid CartId { get; set; }
 
         public Guid ProductId { get; set; }
 
         public int Quantity { get; set; }
-
-        public Guid Id { get; set; }
-
-        [StringLength(50)]
-        public string UserNameCheckId { get; set; }
 
         public virtual Cart Cart { get; set; }
 
