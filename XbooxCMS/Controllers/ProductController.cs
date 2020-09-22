f22fb7d2ae54ce117e4cbbfd4a55772ed61326e8
Sharon Chen <treetwood@gmail.com>
2020-09-22 15:45:13 +0800

    removed upload img function from ProductController to ProductApi

--- XbooxCMS/Controllers/ProductController.cs
+++ XbooxCMS/Controllers/ProductController.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using XbooxCMS.Models;
 using XbooxCMS.ViewModels;
-using System.Data.Entity;
-using System.Diagnostics;
-using System.Data.Entity.Validation;
-using System.IO;
-using System.Web.SessionState;
-using Newtonsoft.Json;
 using Imgur.API.Authentication;
 using System.Net.Http;
 using Imgur.API.Endpoints;
@@ -96,110 +87,6 @@ namespace XbooxCMS.Controllers
             
         }
 
-
-
-
-
-
-
-        [HttpPost]
-        public async Task<ActionResult>  Upload()
-        {
-            //foreach (var file in filepond)
-           // {
-               // if (file != null && file.ContentLength > 0)
-                //{
-                    //var fileName = Path.GetFileName(file.);
-                  //  var path = Path.Combine(Server.MapPath("~Assets/Pics"), fileName);
-                    //要設定傳入product或 productId
-                    //要把Imglink改成可以自動生成id
-                   // productImgs.Add(new ProductImgs() { imgLink = path, ProductId = product.ProductId });
-                   // file.SaveAs(path);
-
-                //}
-           // }
-            //var Files = Request.Files;
-
-            if (Request.Files.Count > 0)
-            {
-
-               // ImgstringList = new List<string>();
-                var files = Request.Files;
-                var File = Request.Files[0];
-                //Imgurapi
-                var apiClient = new ApiClient("0a9f2fb7434821b", "60f9a494f1607de3b90582298fc88c8e29560199");
-                var httpClient = new HttpClient();
-
-                var fileName = files[0].FileName;/*C:\Users\User\source\repos\mikeyeh40709\Xboox\Xboox\Assets\Image\Pics\*/
-               // session["fileName"]=1
-             //從這裡開始可以用imgur
-             //  var path = Path.Combine(Server.MapPath("../Assets/Pics"), fileName);
-
-
-                //   files[0].SaveAs(path);
-
-                var imageEndpoint = new ImageEndpoint(apiClient, httpClient);
-                var imageUpload = await imageEndpoint.UploadImageAsync(File.InputStream);
-                //ImgstringLists.Add(imageUpload.Link);
-                //TempData["imgString"] = ImgstringLists;
-                ProductService.GetImg().Add(imageUpload.Link);
-
-
-                //ViewBag.List = fileNameCollection;
-            }
-            //;
-          
-            //return Json($"fileUpload {Session["fileName"]}");
-            
-            return Json ("OK");
-
-
-      
-        }
-
-     
-        //[HttpPost]
-        //public ActionResult Remove(string ccc)
-        //{
-        //    //var session = httpContext.Session;
-        //    List<Object> abc = new List<Object>();
-            
-        //    abc.Add(Request.Form["ccc"]);
-        //    //abc.Add(Request.Files);
-        //  //  abc.Add(JsonConvert.DeserializeObject<string>(Request.Form["ccc"]));
-        //    abc.Add(Request.Params["abc"]);
-        //    //abc.Add(Request.Params["QUERY_STRING"]);
-            
-            
-           
-        //    //abc.Add(Request.);
-        //    //abc.Add(Request.);
-        //    var files = Request.Files;
-
-
-        //    //從file移除資料
-        //    //接Id 然後再抓Id
-        //    //再處理字串(陣列) 在裡面找該圖的filename然後將其從字串移除
-        //    //  ImgstringList
-        //    //string json = JsonConvert.SerializeObject(files);
-        //    return Json(abc);
-        //}
-
-
-        [HttpPost]
-        public ActionResult RemoveAll()
-        {
-            var service = new ProductService();
-            ///List<string>=null
-
-            service.SetNull();
-
- 
-            return Json("setnull");
-        }
-
-
-     
 
     }
 }
